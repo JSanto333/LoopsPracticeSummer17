f68873d5d82088147c3a7b905a23a87d2392f048
Joe Santoiemmo <JSanto333@gmail.com>
2017-07-06 11:14:53 -0400

    Finished for loop examples

--- LoopsPracticeSummer17/Program.cs
+++ LoopsPracticeSummer17/Program.cs
@@ -32,10 +32,51 @@ namespace LoopsPracticeSummer17
             //}
             //Console.WriteLine("After the loop has run, i now has the value of " + i);
 
-            for (int i = 50; i <= 100; i++)
-            {
-                Console.WriteLine(i);
-            }
+            //for (int i = 50; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //string greeting = "My name is Little Bill.";
+            ////The .Split() method takes a string and turns it into an array of smaller
+            ////strings (substrings). It defaults the split on spaces.
+            //string[] wordsInGreeting = greeting.Split();
+            ////for loop to print the words
+            //for(int i = 0; i < wordsInGreeting.Length; i++)
+            //{
+            //    Console.WriteLine(wordsInGreeting[i]);
+            //}
+
+            //string[] weekDays = { "Monday", "Tuesday", "Wednesday" };
+            //for(int i = 0; i < weekDays.Length; i++)
+            //{
+            //    Console.WriteLine(weekDays[i]);
+            //}
+
+            //string tale = "Once upon a time.";
+            //string[] fairyTale = tale.Split();
+            //Array.Reverse(fairyTale);
+            //for(int i = 0; i < fairyTale.Length; i++)
+            //{
+            //    Console.WriteLine(fairyTale[i]);
+            //}
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int product = numbers[0];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    product = product * numbers[i];
+            //}
+            //Console.WriteLine(product);
+
+            //int product = 1;
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    product = product * i;
+            //}
+            //Console.WriteLine(product);
+
+
         }
     }
 }
